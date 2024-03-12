043175cae3799e2ef47078848841deff9007da75
Oskar Raul Cahueñas <oskar.cahuenas@globant.com>
2024-03-12 00:00:25 -0500

    Added utility methods to ColorEnum and ProductCategoryEnum for validate enumerations

--- src/main/java/com/globant/training/inventorysample/domain/enumerations/ColorEnum.java
+++ src/main/java/com/globant/training/inventorysample/domain/enumerations/ColorEnum.java
@@ -1,5 +1,10 @@
 package com.globant.training.inventorysample.domain.enumerations;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 /**
  * Enumeration for products' colors/
  */
@@ -22,4 +27,33 @@ public enum ColorEnum {
   SALMON,
   YELLOW,
   LAVENDER;
+
+  private static final Map<String, ColorEnum> COLOR_BY_NAME;
+
+  static {
+    COLOR_BY_NAME = new HashMap<>();
+    for (ColorEnum v: values()) {
+      COLOR_BY_NAME.put(v.toString().toUpperCase(), v);
+    }
+  }
+
+  /**
+   * Test if a string with is a valid enumeration value.
+   * @param color String with enumerated number.
+   * @return boolean if color with that name exists in enum.
+   */
+  public static boolean existColor(String color) {
+    return color != null && COLOR_BY_NAME.containsKey(color.toUpperCase());
+  }
+
+  /**
+   * Return string with list of values of this enum.
+   * @return String representation of list of comma-separated values.
+   */
+  public static String valuesList() {
+    return Arrays.stream(values())
+        .sequential()
+        .map(ColorEnum::toString)
+        .collect(Collectors.joining(", "));
+  }
 }
--- src/main/java/com/globant/training/inventorysample/domain/enumerations/ProductCategoryEnum.java
+++ src/main/java/com/globant/training/inventorysample/domain/enumerations/ProductCategoryEnum.java
@@ -7,6 +7,11 @@ package com.globant.training.inventorysample.domain.enumerations;
 
 import lombok.AllArgsConstructor;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 @AllArgsConstructor
 public enum ProductCategoryEnum {
   PANTS_AND_SHORTS("pants and showts"),
@@ -16,4 +21,33 @@ public enum ProductCategoryEnum {
   FOOTWEAR("shoes, boots");
 
   private final String description;
+
+  private static final Map<String, ProductCategoryEnum> CATEGORY_BY_NAME;
+
+  static {
+    CATEGORY_BY_NAME = new HashMap<>();
+    for (ProductCategoryEnum v: values()) {
+      CATEGORY_BY_NAME.put(v.toString().toUpperCase(), v);
+    }
+  }
+
+  /**
+   * Test if a string with is a valid enumeration value.
+   * @param category String with enumerated value as string.
+   * @return boolean if category with that name exists in enum.
+   */
+  public static boolean existsCategory(String category) {
+    return category != null && CATEGORY_BY_NAME.containsKey(category.toUpperCase());
+  }
+
+  /**
+   * Return string with list of values of this enum.
+   * @return String representation of list of comma-separated values.
+   */
+  public static String valuesList() {
+    return Arrays.stream(values())
+        .sequential()
+        .map(ProductCategoryEnum::toString)
+        .collect(Collectors.joining(", "));
+  }
 }
